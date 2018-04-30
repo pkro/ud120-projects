625e14929248bd8543b4933e878b0e5732779d39
pkro <pkrombach@web.de>
2018-04-30 13:02:44 +0200

    initial commit

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,29 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print "accuracy: {}".format(accuracy)
+
+'''
+Output:
+no. of Chris training emails: 7936
+no. of Sara training emails: 7884
+training time: 1.581 s
+prediction time: 0.24 s
+accuracy: 0.973265073948 s
+'''
 
 #########################################################
 
